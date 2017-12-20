6db0cfe8fbde3c1838d6c2d44be8fb27f43ab9e3
Aziz Khoury <bentael@gmail.com>
2017-12-21 00:35:44 +0200

    initial working version

---
+++ index.js
@@ -0,0 +1,51 @@
+
+const generate = (options = {}) => {
+
+    let headerKey = options.headerKey || 'X-Total-Count';
+    let blueprintActions = [...['find'], ...(options.blueprintActions || [])].reduce((hash, key) => { hash[key] = true; return hash; }, {});
+
+    let middleware = function (req, res, next) {
+        let oldSend = res.send;
+
+        res.send = function(data) {
+            if (!req.options || !blueprintActions[req.options.blueprintAction]) {
+                return oldSend.apply(res, arguments);
+            }
+
+            let parseBlueprintOptions = req.options.parseBlueprintOptions || req._sails.config.blueprints.parseBlueprintOptions;
+            if (!parseBlueprintOptions) {
+                sails.log.warn('[sails-count-middleware] middleware ignored, parseBlueprintOptions function not found.');
+                return oldSend.apply(res, arguments);
+            }
+
+            let queryOptions = parseBlueprintOptions(req);
+            let Model = req._sails.models[queryOptions.using] ;
+            let criteria = Object.assign({}, queryOptions.criteria);
+
+            delete criteria.limit;
+            delete criteria.skip;
+            delete criteria.sort;
+
+            return Model.count(criteria)
+                .then(
+                    (count) => {
+                        res.set(headerKey, count);
+                    })
+                .catch(
+                    (err) => {
+                        throw err;
+                    })
+                .finally(
+                    () => {
+                        return oldSend.apply(res, arguments);
+                    }
+                );
+        };
+        next();
+    };
+
+    middleware.generate = generate;
+    return middleware;
+};
+
+module.exports = generate();
---
+++ readme.md
@@ -0,0 +1,56 @@
+
+# sails-count-middleware
+Find the count by criteria by adding an 'X-Total-Count' header for all blueprints 'find' calls
+
+# Sails version support
+It only supports __Sails 1.x__
+
+# Usage
+
+```
+npm install sails-count-middleware --save
+```
+Then in your `config/http.js`
+
+```javascript
+middleware: {
+// ....
+    // require it with whatever name you want
+    addXTotalCount: require('sails-count-middleware'),
+
+    // then add it to the order[] array
+    order: [
+      // ...
+      'addXTotalCount',
+      // ...
+     ]
+```
+
+Then make a `GET` call to a `find` blueprint action, with any criteria, even with limit
+```bash
+curl -sSL -D - localhost:1337/user?name=jane&limit=1
+```
+Result
+```
+HTTP/1.1 200 OK
+X-Powered-By: Sails <sailsjs.com>
+Content-Type: application/json; charset=utf-8
+X-Total-Count: 30   <------------------------------- Find the header here with the total counts
+Content-Length: 2942
+ETag: W/"b7e-riSmb6LicvHvEh5sQ+oJ2rzxbb8"
+Date: Wed, 20 Dec 2017 22:22:06 GMT
+Connection: keep-alive
+[
+  {..}  // Only 1 result though, since limit=1
+]
+```
+
+# Options
+
+There are 2 options that you change, just call the `generate()` function instead
+```javascript
+    addXTotalCount: require('sails-count-middleware').generate({
+        blueprintActions: ['find'], // default
+        headerKey: 'X-Total-Count' // default
+    }),
+```
