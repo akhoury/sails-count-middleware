19256f0772744a42f9d245fbc85b602e62ba32e7
Aziz Khoury <bentael@gmail.com>
2017-12-21 04:18:06 +0200

    added support for faster processing if options are avalable

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 
+
+
 const generate = (options = {}) => {
 
     let headerKey = options.headerKey || 'X-Total-Count';
     let blueprintActions = [...['find'], ...(options.blueprintActions || [])].reduce((hash, key) => { hash[key] = true; return hash; }, {});
 
     let middleware = function (req, res, next) {
-        // todo: I really really didn't want to do that
-        // but at the time of calling the middleware, req.options.blueprintAction was undefined
-        // only after it gets processed by sails that these options are added
-        let oldSend = res.send;
+        let now = !!(req.options && req.options.blueprintAction);
+        let oldSendOrNext;
 
-        res.send = function(data) {
+        // if we have options, execute now
+        if (now) {
+            // wrap next with a function so it won't get affected with the .apply calls below
+            oldSendOrNext = () => next();
+        } else {
+            // else, save the req.send to override it, so addCountThenSendOrNext can execute later
+
+            // todo: I really really didn't want to do that
+            // but at the time of calling the middleware, req.options.blueprintAction was undefined
+            // only after it gets processed by sails that these options are added
+            oldSendOrNext = res.send;
+        }
+
+        let addCountThenSendOrNext = function(data) {
             if (!req.options || !blueprintActions[req.options.blueprintAction]) {
-                return oldSend.apply(res, arguments);
+                return oldSendOrNext.apply(res, arguments);
             }
 
             let parseBlueprintOptions = req.options.parseBlueprintOptions || req._sails.config.blueprints.parseBlueprintOptions;
             if (!parseBlueprintOptions) {
                 sails.log.warn('[sails-count-middleware] middleware ignored, parseBlueprintOptions function not found.');
-                return oldSend.apply(res, arguments);
+                return oldSendOrNext.apply(res, arguments);
             }
 
             let queryOptions = parseBlueprintOptions(req);
@@ -40,14 +53,21 @@ const generate = (options = {}) => {
                     })
                 .finally(
                     () => {
-                        return oldSend.apply(res, arguments);
+                        return oldSendOrNext.apply(res, arguments);
                     }
                 );
         };
-        next();
+
+        if (now) {
+            addCountThenSendOrNext();
+        } else {
+            res.send = addCountThenSendOrNext;
+            next();
+        }
     };
 
     middleware.generate = generate;
+
     return middleware;
 };
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 
 # sails-count-middleware
-While paginating, find the total count by criteria, via an added `X-Total-Count` header to all blueprints `find` requests 
+While paginating, find the total count by criteria, via an added `X-Total-Count` header to all blueprints `find` requests
 
 ![screen shot 2017-12-21 at 3 31 10 am](https://user-images.githubusercontent.com/1398375/34236288-7bd4f658-e5ff-11e7-87b7-53be1433f272.png)
 
@@ -8,7 +8,7 @@ While paginating, find the total count by criteria, via an added `X-Total-Count`
 # Sails version support
 It only supports __Sails 1.x__
 
-# Usage
+# Basic Usage
 
 ```
 npm install sails-count-middleware --save
@@ -31,11 +31,25 @@ middleware: {
      ]
 ```
 
+# Advanced Usage
+You can create a policy, say we called it `addCount.js`
+```javascript
+ module.exports = require('sails-count-middleware')
+```
+Then in `config/policies.js` you can specify which `find` call will get augmented with the count header.
+```javascript
+UserController: {
+    'find': ['isLoggedIn', 'addCount'],
+}
+```
+
+# Result
 Then make a `GET` call to a `find` blueprint action, with any criteria, even with limit
+#### Request
 ```bash
 curl -sSL -D - localhost:1337/user?name=jane&limit=1
 ```
-Result
+#### Response
 ```
 HTTP/1.1 200 OK
 X-Powered-By: Sails <sailsjs.com>
@@ -50,7 +64,7 @@ Connection: keep-alive
 ]
 ```
 
-# Options
+# Extra Options
 
 There are 2 options that you change, just call the `generate()` function instead
 ```javascript
